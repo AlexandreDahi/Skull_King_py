f2db36d4de171987cd0859ca64fb8f764f4862a1
AlexandreDahi <ad.alexandredahi@gmail.com>
2026-03-13 15:18:23 +0100

    test pour le serveur

--- frontend/src/app/service/websocket/websocket.service.ts
+++ frontend/src/app/service/websocket/websocket.service.ts
@@ -33,8 +33,8 @@ export class WebSocketService {
         this.roomUuid = roomUuid
         this.playerToken = playerToken
 
-        // const wsUrl = `ws://localhost:8000/ws/rooms/${roomUuid}?&player_token=${playerToken}`;
-        const wsUrl = `ws://15.237.60.236:8000/ws/rooms/${roomUuid}?&player_token=${playerToken}`;
+        const wsUrl = `ws://localhost:8000/ws/rooms/${roomUuid}?&player_token=${playerToken}`;
+        // const wsUrl = `ws://15.237.60.236:8000/ws/rooms/${roomUuid}?&player_token=${playerToken}`;
         
 
         console.log('🔌 WebSocket joinRoom:', {
